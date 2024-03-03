94ad7ab04ef2c1b493e8afd001fc4d43a25ab8a5
Geethu Sebastian <geethugspdev@gmail.com>
2024-03-03 13:31:28 +0530

    first

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <div className="flex px-2 py-2 flex-col justify-between">
+      <div className="flex px-2 py-2 flex-col justify-between w-full h-[10%]">
         <div className="flex w-[10%] px-1">
           <Image
             src="/Logo.svg"
@@ -15,9 +15,14 @@ const Navbar = () => {
             height={20}
             priority
           />
-          <p>Mindful Moves</p>
-          <Link href="/">Home</Link>
-            <Link href="/reccomendation">Recommendation</Link>
+          
+          <p className="text-xl text-red-400 font-semibold w-[30%]">Mindful Moves</p>
+          <div>
+          <Link href="/" className="flex font-semibold text-xl px-2">Home</Link>
+          </div>
+          <div>
+            <Link href="/reccomendation" className="flex font-semibold text-xl px-2">Recommendation</Link>
+            </div>
         </div>
       </div>
     </>
